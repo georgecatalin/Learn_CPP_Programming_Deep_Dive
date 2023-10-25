ee27f775ae6a42d7fe0568302dd5837da8fb05fa
georgecatalin <george.calin@gmail.com>
2023-10-25 12:44:37 +0300

    multidimensional arrays

---
+++ Learn_CPP_Programming_Deep_Dive/Section 8 Arrays/Multidimensional_arrays/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int A[2][3];
+
+    for(auto& x: A)
+    {
+        for(auto& y: x)
+        {
+            cin>>y;
+        }
+    }
+
+    for(auto& x: A)
+    {
+        for(auto& y: x)
+        {
+            cout<<y<<" ";
+        }
+
+        cout<<endl;
+    }
+
+
+}
