985a5f98078a5df2244b75ed84cf740752adee25
George Calin <george.calin@gmail.com>
2023-11-07 20:14:36 +0200

    understand constructors in inheritance

---
+++ Learn_CPP_Programming_Deep_Dive/Section 13 Inheritance/Constructors_in_inheritance/Constructors_in_inheritance/Constructors_in_inheritance/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Constructors_in_inheritance
+//
+//  Created by George Catalin on 07.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
